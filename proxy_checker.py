169225e45b96df5637336c56c036d43e841906ff
Danyil Kuzminskyi <74465843+DanielKuzminskij@users.noreply.github.com>
2024-11-04 14:39:23 -0800

    Update proxy_checker.py

--- proxy_checker.py
+++ proxy_checker.py
@@ -1,10 +1,9 @@
 import requests
 
-def check_proxy(proxy):
-
+def check_proxy(proxy_url):
     proxies = {
-        'http': f'http://{proxy}',
-        'https': f'http://{proxy}'
+        'http': proxy_url,
+        'https': proxy_url
     }
     url = 'https://api.ipify.org/?format=json'  # URL для перевірки проксі
 
@@ -19,17 +18,39 @@ def check_proxy(proxy):
         return False
 
 def load_proxies_from_file(file_path):
-
     proxies = []
     try:
         with open(file_path, 'r') as file:
-            proxies = [line.strip() for line in file.readlines() if line.strip()]
+            for line in file.readlines():
+                line = line.strip()
+                if line:
+                    # Визначення типу проксі за префіксом або структурою рядка
+                    if line.startswith("socks5://") or line.startswith("http://") or line.startswith("https://"):
+                        proxy = line
+                    elif '@' in line:
+                        # Якщо логін і пароль включені, припускаємо тип socks5
+                        parts = line.split('@')
+                        auth = parts[0].split(':')
+                        host = parts[1]
+
+                        # Перевірка наявності логіна і пароля
+                        if len(auth) == 2:
+                            username = auth[0]
+                            password = auth[1]
+                            proxy = f"socks5://{username}:{password}@{host}"
+                        else:
+                            proxy = f"socks5://{host}"
+                    else:
+                        # Якщо тип не вказано, припускаємо socks5 без авторизації
+                        proxy = f"socks5://{line}"
+
+                    proxies.append(proxy)
     except FileNotFoundError:
         print(f"Файл '{file_path}' не знайдено.")
     return proxies
 
 def main():
-    file_path = 'D:\Desktop\proxy_list.txt'  # Шлях до файлу з проксі
+    file_path = 'D:\\Desktop\\proxy_list.txt'  # Шлях до файлу з проксі
     proxies = load_proxies_from_file(file_path)
 
     if not proxies:
@@ -37,12 +58,12 @@ def main():
         return
 
     print("Перевірка проксі:")
-    for proxy in proxies:
-        ip = check_proxy(proxy)
+    for proxy_url in proxies:
+        ip = check_proxy(proxy_url)
         if ip:
-            print(f"Проксі {proxy} працює коректно. IP: {ip}")
+            print(f"Проксі {proxy_url} працює коректно. IP: {ip}")
         else:
-            print(f"Проксі {proxy} не доступний або не працює.")
+            print(f"Проксі {proxy_url} не доступний або не працює.")
 
 if __name__ == "__main__":
     main()
